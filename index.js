d61dc648fce44d1537496e57df727884e2ea4abd
손량 <loop.infinitely@gmail.com>
2019-05-23 14:55:03 +0900

    added button handle

--- index.js
+++ index.js
@@ -58,8 +58,8 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history;
-    const current = history[history.length - 1];
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[this.state.stepNumber];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -69,6 +69,7 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
